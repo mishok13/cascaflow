a627f679efcca9dec166bc53fd474838f44b43a4
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-01-27 13:48:09 +0100

    Enable running from command line

--- project.clj
+++ project.clj
@@ -2,4 +2,7 @@
   :description "Clojure wrapper for Google's Dataflow"
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [com.google.cloud.dataflow/google-cloud-dataflow-java-sdk-all "0.3.150109"]]
+  :aot :all
+  :main ^:skip-aot cascaflow.dataflow.wordcount)
