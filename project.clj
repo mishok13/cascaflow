bf6a3a9253e4521e58cd12ec70a7234d059a2bf0
Andrii V. Mishkovskyi <gdmfsob@mishkovskyi.net>
2015-01-27 09:48:10 +0100

    Kicking off the project

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject cascaflow "0.1.0-SNAPSHOT"
+  :description "Clojure wrapper for Google's Dataflow"
+  :license {:name "MIT"
+            :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.6.0"]])
---
+++ src/cascaflow/core.clj
@@ -0,0 +1,1 @@
+(ns cascaflow.core)
